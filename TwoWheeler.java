8231ef5a4d529a4f479fa9684f8bb77e96df8deb
GowriShankarAR <gowrishankar.ar.14@gmail.com>
2022-06-16 17:24:48 +0530

    Create TwoWheeler.java

---
+++ TwoWheeler.java
@@ -0,0 +1,33 @@
+package week2.Day2;
+
+public class TwoWheeler {
+	public static void main(String[] args) {
+		
+		TwoWheeler bikeObject = new TwoWheeler ();
+		
+		int noOfWheels2 = bikeObject.noOfWheels;
+		System.out.println(noOfWheels2);
+		
+		short noOfMirrors2 = bikeObject.noOfMirrors;
+		System.out.println(noOfMirrors2);
+		
+		long chassisNumber2 = bikeObject.chassisNumber;
+		System.out.println(chassisNumber2);
+		
+		boolean isPunctured2 = bikeObject.isPunctured;
+		System.out.println(isPunctured2);
+		
+		String bikeName2 = bikeObject.bikeName;
+		System.out.println(bikeName2);
+		
+		double runningKM2 = bikeObject.runningKM;
+		System.out.println(runningKM2);
+		
+	}	
+		int noOfWheels = 2;
+		short noOfMirrors = 2;
+		long chassisNumber = 12678716;
+		boolean isPunctured = false;
+		String bikeName = "Pulsar";
+		double runningKM = 1233 ;
+	}
